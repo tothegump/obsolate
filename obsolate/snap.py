c7cc1e97c86117c51813966c6293de71b5edfbc8
tothegump <tothegump@gmail.com>
2017-02-19 18:36:03 +0800

    add dynamic url

--- obsolate/snap.py
+++ obsolate/snap.py
@@ -1,9 +1,11 @@
 # -*- coding:utf8 -*-
+import re
+
 REQUEST_MAPPING = {
-    'GET': {},
-    'POST': {},
-    'PUT': {},
-    'DELETE': {},
+    'GET': [],
+    'POST': [],
+    'PUT': [],
+    'DELETE': [],
 }
 
 
@@ -15,10 +17,11 @@ def find_matching_url(environ, start_response):
     request_method = environ.get('REQUEST_METHOD', 'GET')
     if request_method not in REQUEST_MAPPING:
         raise NotFound('not support method :(')
-    path = environ.get('PATH_INFO', '')
-    for url, method in REQUEST_MAPPING[request_method].items():
-        if url == path:
-            return method
+    path = add_slash(environ.get('PATH_INFO', ''))
+    for url_set in REQUEST_MAPPING[request_method]:
+        match = url_set[0].search(path)
+        if match is not None:
+            return (url_set, match.groupdict())
     raise NotFound('Sorry, you got nothing!')
 
 
@@ -29,11 +32,12 @@ def not_found(environ, start_response):
 
 def handle_request(environ, start_response):
     try:
-        callback = find_matching_url(environ, start_response)
+        (url_set, url, callback), kwargs = find_matching_url(environ, start_response)
+        print (url_set, url, callback)
     except NotFound:
         return not_found(environ, start_response)
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return callback()
+    return callback(**kwargs)
 
 
 def take_a_snap():
@@ -49,14 +53,20 @@ def take_a_snap():
 
 
 # decorators
+def add_slash(url):
+    if not url.endswith('/'):
+        url = '{}/'.format(url)
+    return url
+
+
 def get(url):
-
     def wrapped(func):
-
         def new(*args, **kwargs):
             return func(*args, **kwargs)
         # Register.
-        REQUEST_MAPPING['GET'][url] = new
+        re_url = re.compile('^{path}$'.format(path=add_slash(url)))
+        REQUEST_MAPPING['GET'].append((re_url, url, new))
+        return new
     return wrapped
 
 
@@ -64,6 +74,8 @@ def post(url):
     def wrapped(func):
         def new(*args, **kwargs):
             return func(*args, **kwargs)
+        re_url = re.compile('^{path}$'.format(path=add_slash(url)))
+        REQUEST_MAPPING['POST'].append((re_url, url, new))
         return new
     return wrapped
 
@@ -76,6 +88,24 @@ def index():
 @get('/hello')
 def greeting():
     return 'hello world'
+
+
+@post('/hello')
+def greeting_post(**kwargs):
+    print kwargs
+    return 'hi, postman'
+
+
+@get('/hello/(?P<name>\w+)')
+def personal_greeting(name=', world'):
+    return 'Hello %s!' % name
+
+
+@post('/hello/(?P<name>\w+)')
+def greeting_post(name, **kwargs):
+    print kwargs
+    return 'hi, postman, I am {name}'.format(name=name)
+
 
 if __name__ == "__main__":
     take_a_snap()
